3e8c870898943dead685e7777394aac7115e0c70
Shady-Prakash <shadyprakash8@gmail.com>
2025-04-24 09:40:40 +0100

    query multiple elements

---
+++ src/components/Skills/skills.test.tsx
@@ -0,0 +1,19 @@
+import { describe, expect, test } from "vitest";
+import Skills from "./skills";
+import { render, screen } from "@testing-library/react";
+
+describe("Skills", () => {
+  const skills = ['HTML', 'CSS', 'JavaScript'];
+
+  test('renders correctly', () => {
+    render(<Skills skills={skills}/>)
+    const listElement = screen.getByRole('list')
+    expect(listElement).toBeInTheDocument()
+  })
+
+  test('renders a list of skills', () => {
+    render(<Skills skills={skills}/>)
+    const listItemElements = screen.getAllByRole('listitem')
+    expect(listItemElements).toHaveLength(skills.length);
+  })
+})
