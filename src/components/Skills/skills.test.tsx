6bd265aa59adebd59a8622b7aa67c2d15b9b230b
Shady-Prakash <shadyprakash8@gmail.com>
2025-04-26 08:58:02 +0545

    queryByRole method

--- src/components/Skills/skills.test.tsx
+++ src/components/Skills/skills.test.tsx
@@ -6,14 +6,30 @@ describe("Skills", () => {
   const skills = ['HTML', 'CSS', 'JavaScript'];
 
   test('renders correctly', () => {
-    render(<Skills skills={skills}/>)
-    const listElement = screen.getByRole('list')
-    expect(listElement).toBeInTheDocument()
-  })
+    render(<Skills skills={skills}/>);
+    const listElement = screen.getByRole('list');
+    expect(listElement).toBeInTheDocument();
+  });
 
   test('renders a list of skills', () => {
-    render(<Skills skills={skills}/>)
-    const listItemElements = screen.getAllByRole('listitem')
+    render(<Skills skills={skills}/>);
+    const listItemElements = screen.getAllByRole('listitem');
     expect(listItemElements).toHaveLength(skills.length);
+  });
+
+  test('renders Login button', () => {
+    render(<Skills skills={skills}/>);
+    const loginButton = screen.getByRole('button', {
+      name: 'Login'
+    });
+    expect(loginButton).toBeInTheDocument();
+  })
+
+  test('Start Learing button is not rendered', () => {
+    render(<Skills skills={skills}/>);
+    const startLearningButton = screen.queryByRole('button', {
+      name: 'Start learning'
+    });
+    expect(startLearningButton).not.toBeInTheDocument();
   })
 })
