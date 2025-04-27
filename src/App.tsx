dfebdaefbf874239c4a3bbb4e8b75ee0a09ea2be
Shady-Prakash <shadyprakash8@gmail.com>
2025-04-27 09:36:17 +0545

    pointer interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Application from './components/Application/application'
+import Counter from './components/Counter/counter'
 
 function App() {
 
   return (
     <div className="App">
       <Application/>
+      <Counter/>
     </div>
   )
 }
